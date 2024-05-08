685c51d6cff543f8255db627e7a3dc1c1911bacf
Zubair Islam Epty <eptyt4@gmail.com>
2024-05-08 14:43:17 +0600

    1.0.4

---
+++ Runtime/Core/CutsceneController.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using System.Linq;
+using Studio23.SS2.Cutscenesystem.Data;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+
+namespace Studio23.SS2.Cutscenesystem.Core
+{
+    public class CutsceneController : MonoBehaviour
+    {
+        public static CutsceneController instance;
+        public PlayableDirector Director;
+        private readonly float DampingValue = 0.1f;
+        private bool _isPaused;
+
+        private List<TimelineClip> ActivationClip;
+
+        public UnityEvent OnPageAdvance;
+        public UnityEvent OnPageSkip;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+
+        /// <summary>
+        ///     Used to advance pages, skipcount parameter used to advance to the selected page column if want to skip to that page
+        ///     column
+        /// </summary>
+        /// <param name="skipCount"></param>
+        public void AdvancePage(int skipCount)
+        {
+            if (_isPaused)
+            {
+                Director.Play();
+                _isPaused = false;
+                Debug.Log("Timeline resumed");
+            }
+            else
+            {
+                // Access the timeline asset from the director
+                var timeline = Director.playableAsset as TimelineAsset;
+                if (timeline == null) return;
+
+                var currentTime = Director.time;
+                var tracks = timeline.GetOutputTracks();
+                var cutSceneTracks = tracks.OfType<CutsceneTrack>().ToList();
+                var activeTracks = tracks.OfType<ActivationTrack>().ToList();
+
+                foreach (var activeTrack in activeTracks)
+                {
+                    if (activeTrack.start > currentTime || activeTrack.end < currentTime) continue;
+                    var shouldSkip = false;
+                    ActivationClip = activeTrack.GetClips().ToList();
+                    var currentClip = skipCount == -1 ? ActivationClip.Count : skipCount;
+                    Director.time = activeTrack.end - DampingValue;
+                    Director.Evaluate();
+                    shouldSkip = true;
+
+                    if (shouldSkip)
+                    {
+                        _isPaused = true;
+                        OnPageAdvance?.Invoke();
+                        Director.Pause();
+                        Debug.Log($"Director time now {Director.time} and track duration {activeTrack.duration}");
+                        break; // Break since we've handled the skip for the active track
+                    }
+                }
+
+                foreach (var track in cutSceneTracks)
+                {
+                    if (track.start > currentTime || track.end < currentTime) continue;
+                    var clips = track.GetClips().ToList();
+                    var currentClip = skipCount == -1 ? clips.Count : skipCount;
+
+                    for (var i = 0; i < currentClip; i++)
+                    {
+                        var cut = clips[i].asset as CutsceneClip;
+                        if (cut != null) cut.CutsceneBehaviour.ForceAlpha();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Skip to the whole timeline track
+        /// </summary>
+        public void SkipPage()
+        {
+            var timelineAsset = Director.playableAsset as TimelineAsset;
+            if (timelineAsset != null)
+            {
+                var markers = timelineAsset.markerTrack.GetMarkers().ToArray();
+                OnPageSkip?.Invoke();
+                Director.time = timelineAsset.duration;
+            }
+        }
+    }
+
+}
+
---
+++ Runtime/Core/CutscenePlayableBehaviour.cs
@@ -0,0 +1,57 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Studio23.SS2.Cutscenesystem.Core
+{
+    internal class CutscenePlayableBehaviour : PlayableBehaviour
+    {
+        public GameObject Page;
+        public float StartAlpha;
+        public float EndAlpha;
+        internal float CurrentAlpha;
+
+        private bool _firstFrameHappened;
+
+        
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            if (Page != null)
+            {
+                float progress = (float)(playable.GetTime() / playable.GetDuration());
+                CurrentAlpha = Mathf.Lerp(StartAlpha, EndAlpha, progress);
+                var renderer = Page.GetComponent<CanvasGroup>();
+                renderer.alpha = CurrentAlpha;
+                if (!_firstFrameHappened)
+                {
+                    renderer.alpha = StartAlpha;
+                    _firstFrameHappened = true;
+                }
+            }
+        }
+
+        public override void OnPlayableDestroy(Playable playable)
+        {
+            _firstFrameHappened = false;
+            if(Page != null) Page.GetComponent<CanvasGroup>().alpha = StartAlpha;
+        }
+
+        public void Initialize(GameObject targetPage, float startAlpha, float endAlpha)
+        {
+            Page = targetPage;
+            StartAlpha = startAlpha;
+            EndAlpha = endAlpha;
+            Page.GetComponent<CanvasGroup>().alpha = startAlpha;
+        }
+
+        /// <summary>
+        /// Make the remaining pages transparency to Maximum
+        /// </summary>
+        public void ForceAlpha()
+        {
+            Page.GetComponent<CanvasGroup>().alpha = 1f;
+        }
+
+    }
+
+}
